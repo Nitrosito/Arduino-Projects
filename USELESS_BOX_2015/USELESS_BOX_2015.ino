65cde48d73734ba37fc23146b8a58e11443b4e3b
Nitrosito <nitrosito@gmail.com>
2015-09-24 14:50:19 +0200

    Fix the last commit

---
+++ USELESS_BOX_2015/USELESS_BOX_2015.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+Servo myservo;  // Creación del objeto servo para poder controlarlo
+int interruptor=4;
+
+void setup(){
+  myservo.attach(3);  //Inicializa servo en pin3
+  pinMode(interruptor,INPUT);
+}
+
+void loop() { 
+  if(digitalRead(interruptor)==0){
+    myservo.write(90);
+    delay(250);
+    myservo.write(1);
+    delay(250);
+  }
+
+  else{
+   myservo.write(1);
+   delay(100);
+  }
+} 
